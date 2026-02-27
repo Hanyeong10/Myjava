6fe2a8f95194652fe7a2be7af596769109197a1d
hanyeong10 <hanyeong10@gmail.com>
2026-02-27 16:40:20 +0900

    second commit

--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -3,6 +3,8 @@ package ch01_variable_operator;
 public class MyPrint {
     static void main() {
         System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("히히");
         System.out.println("박박");
         System.out.println();
         System.out.println("철");
